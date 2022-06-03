dcd578fa827ab3ce31118718f78aadd324229711
PauloDev1 <pc394108@gmail.com>
2022-06-03 17:17:59 -0300

    Versão base

---
+++ script.js
@@ -0,0 +1,42 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const cloud = document.querySelector('.cloud');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudPosition = +window.getComputedStyle(cloud).left.replace('px', '')
+
+    if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 60) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'assets/imgs/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '30px';
+
+        cloud.style.animation = 'none';
+        cloud.style.left = `${cloudPosition}px`;
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
